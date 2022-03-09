40c84459eef528cf42ed9d44804b32a71d5aa3b7
Praful <prafulrakhade02@gmail.com>
2022-03-09 09:58:22 +0530

    [praful] add: UC-1-EmpAttendance

---
+++ EmployeeComp/EmployeeComp/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComp
+{
+    public class Class1
+    {
+        int isPresent = 1;
+        int empCheck;
+        Random random = new Random();
+        public void Attendance()
+        {
+            int randomCheck = random.Next(2);
+            if(randomCheck==isPresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeComp/EmployeeComp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeComp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Class1 check = new Class1();
+            check.Attendance();
+        }
+    }
+}
